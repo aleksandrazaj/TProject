7a8d63c36dbe24fc04b1d0c07f0782a794313444
kuba <jkdejnek@gmail.com>
2019-02-28 11:05:44 +0100

    message

---
+++ store.js
@@ -0,0 +1,11 @@
+var album = {
+    artistName: 'Kendrick Lamar',
+    albumName: 'DAMN.',
+    newRelease: false,
+
+    artistAlbum function(){
+        return this.artistName + " " + "-" + " " + this.albumName;
+    }
+}
+
+artistAlbum();
