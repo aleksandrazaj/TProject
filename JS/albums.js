120e1899a7dbb7ce187fd841f464feba79b0bde2
kuba <jkdejnek@gmail.com>
2019-03-29 10:44:55 +0100

    artists

--- JS/albums.js
+++ JS/albums.js
@@ -22,29 +22,27 @@ imgage.src = "./covers/damn.jpg";
 damn.appendChild(image);*/
 
 class Album {
-    constructor(title, artist, genre, date, cover, artistImg){
+    constructor(title, artist, genre, date, cover, albumId){
     this.title = title
     this.artist = artist
     this.genre = genre
     this.date = date
     this.cover = cover
-    this.artistImg = artistImg }
+    this.albumId = albumId }
 
     createHTML() {
         return "<div class='column'><img style='width:100%' src=" + this.cover + "></img><p>" + this.title + " " + this.artist + "</p></div>"
     }
 
-    createArtist() {
-        return "<div class='column'><img style='width:100%' src=" + this.artistImg + "></img><p>" + this.artist + "</p></div>"
-    }
+    
 }
 
-var album1 = new Album("DAMN.","Kendrick Lamar","Hip Hop",false, "./covers/damn.png", "./artists/kendrick.jpg" )
-var album2 = new Album("B4.DA.$$","Joey Bada$$","Hip Hop",false, "./covers/badass.jpg", "./artists/joey.jpg")
-var album3 = new Album("Self-Titled","Kids See Ghosts","Hip Hop", true, "./covers/ksg.jpg", "./artists/kanyex2.jpg")
-var album4 = new Album("Slime Season 3","Young Thug","Hip Hop",false, "./covers/slime_season_3.jpg", "./artists/young.jpg")
-var album5 = new Album("Rodeo","Travis Scott","Hip Hop",false, "./covers/rodeo.jpg", "./artists/travisscott.jpg")
-var album6 = new Album("Channel Orange","Frank Ocean","Neo Soul",false, "./covers/channel_orange.jpg", "./artists/frank.jpg")
+var album1 = new Album("DAMN.","Kendrick Lamar","Hip Hop",false, "./covers/damn.png", "./artists/kendrick.jpg", "01" )
+var album2 = new Album("B4.DA.$$","Joey Bada$$","Hip Hop",false, "./covers/badass.jpg", "./artists/joey.jpg", "02")
+var album3 = new Album("Self-Titled","Kids See Ghosts","Hip Hop", true, "./covers/ksg.jpg", "./artists/kanyex2.jpg", "03")
+var album4 = new Album("Slime Season 3","Young Thug","Hip Hop",false, "./covers/slime_season_3.jpg", "./artists/young.jpg", "04")
+var album5 = new Album("Rodeo","Travis Scott","Hip Hop",false, "./covers/rodeo.jpg", "./artists/travisscott.jpg", "05")
+var album6 = new Album("Channel Orange","Frank Ocean","Neo Soul",false, "./covers/channel_orange.jpg", "./artists/frank.jpg", "06")
 
 
 var albums = [album1, album2, album3, album4, album5, album6];
@@ -55,8 +53,8 @@ var albums = [album1, album2, album3, album4, album5, album6];
 
 
 
-albums.push(new Album("Astroworld", "Travis Scott", "Hip Hop", true, "./covers/astroworld.jpg"))
-albums.push(new Album("Lemonade", "Beyonce", "R&B", false, "./covers/lemonade.jpg", "./artists/beyonce.jpg"))
+albums.push(new Album("Astroworld", "Travis Scott", "Hip Hop", true, "./covers/astroworld.jpg", "07"))
+albums.push(new Album("Lemonade", "Beyonce", "R&B", false, "./covers/lemonade.jpg", "./artists/beyonce.jpg", "08"))
 
 function displayAlbums() {
     for (let i = 0; i < albums.length; i++) {
--- JS/artists.js
+++ JS/artists.js
@@ -1,8 +1,60 @@
+// var uniqueArtists = [];
+// for (let i = 0; i < albums.length; i++) {
+
+
+    
+//     // if (uniqueArtists.length === 0) {
+//     //     uniqueArtists.push(albums[i])
+//     // }
+
+//     // for(let n = 0; n < uniqueArtists.length; n++) {
+        
+//     //     if (!uniqueArtists[n].artist === albums[i].artist) {
+//     //         uniqueArtists.push(albums[i])
+//     //     }
+//     // }
+
+// }
+
+// var albumMap = albums.map((item) => {
+//     return item.artist
+// })
+
+// var uniqueAlbums = albumMap.filter(function (item, index) {
+//     return albumMap.indexOf(item) >= index
+// })
+
+
+
+
+
+
+// console.log(uniqueAlbums);
+
+class Artist {
+    constructor(name, pic,) {
+    this.name = name
+    this.pic = pic }
+
+    createArtist() {
+        return "<div class='column'><img style='width:100%' src=" + this.pic + "></img><p>" + this.name + "</p></div>"
+    }
+}
+
+var artist1 = new Artist("Kendrick Lamar", "./artists/kendrick.jpg")
+var artist2 = new Artist("Joey Bada$$", "./artists/joey.jpg")
+var artist3 = new Artist("Kids See Ghosts", "./artists/kanyex2.jpg")
+var artist4 = new Artist("Young Thug", "./artists/young.jpg")
+var artist5 = new Artist("Travis Scott", "./artists/travisscott.jpg")
+var artist6 = new Artist("Frank Ocean", "./artists/frank.jpg")
+var artist7 = new Artist("Beyonce", "./artists/beyonce.jpg")
+
+var artists = [artist1, artist2, artist3, artist4, artist5, artist6, artist7]
 
 function displayArtists() {
     var html = "";
-    for (i = 0; i < albums.length; i++) {
-        html += albums[i].createArtist();
+    for (i = 0; i < artists.length; i++) {
+        html += artists[i].createArtist();
     }
     
     var display = document.getElementById("showArtists");
@@ -18,3 +70,7 @@ displayArtists();
         return artists.indexOf(item) >= index;
     })
     */
+
+   //const names = ['John', 'Paul', 'George', 'Ringo', 'John'];
+
+   //let unique = [...new Set(names)];
