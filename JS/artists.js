cb3da6f190dcb14a977e1b689186b116e96004eb
kuba <jkdejnek@gmail.com>
2019-03-29 14:49:42 +0100

    new albums

--- JS/artists.js
+++ JS/artists.js
@@ -11,13 +11,15 @@ class Artist {
 
 var artist1 = new Artist("Kendrick Lamar", "./artists/kendrick.jpg", "01")
 var artist2 = new Artist("Joey Bada$$", "./artists/joey.jpg", "02")
-var artist3 = new Artist("Kids See Ghosts", "./artists/kanyex2.jpg", "03")
+var artist3 = new Artist("Kids See Ghosts", "./artists/ksg.jpg", "03")
 var artist4 = new Artist("Young Thug", "./artists/young.jpg", "04")
 var artist5 = new Artist("Travis Scott", "./artists/travisscott.jpg", "05")
 var artist6 = new Artist("Frank Ocean", "./artists/frank.jpg", "06")
 var artist7 = new Artist("Beyonce", "./artists/beyonce.jpg", "07")
 
 var artists = [artist1, artist2, artist3, artist4, artist5, artist6, artist7]
+artists.push(new Artist("Mick Jenkins", "./artists/mickjenkins.jpg", "08"))
+artists.push(new Artist("Skepta", "./artists/skepta.jpg", "09"))
 
 //Function to display all artist
 function displayArtists() {
